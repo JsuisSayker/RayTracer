e4be6c3d38e35576f12ff5a71b139bc740be4659
Tom Mendy <tom.mendy@epitech.eu>
2024-05-08 18:29:46 +0200

    [WIP] add metal

---
+++ interface/include/raytracer/IMaterial.hpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2024
+** RayTracer
+** File description:
+** IMaterial
+*/
+
+#ifndef IMATERIAL_HPP_
+#define IMATERIAL_HPP_
+
+#include <PrimitivesRecord.hpp>
+#include <Ray.hpp>
+#include <Vector3D.hpp>
+
+class IMaterial {
+  public:
+    IMaterial() {};
+    virtual ~IMaterial() = default;
+
+  virtual bool scatter(const RayTracer::Ray &r_in,
+                       PrimitivesRecord &rec,
+                       Math::Vector3D &attenuation,
+                       RayTracer::Ray &scattered) const = 0;
+  protected:
+    Math::Vector3D albedo;
+};
+
+
+#endif /* !IMATERIAL_HPP_ */
