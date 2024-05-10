707a1514b1baa3ca0fc6ca6904b209a5f44ecd36
Tom Mendy <tom.mendy@epitech.eu>
2024-05-10 21:14:40 +0200

    [FIX] format

--- format.sh
+++ format.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-clang-format -i $(find $(pwd)/plugins -maxdepth 5 -type f -not -path '*/\.*' | grep ".[c|h][p][p]$")
-clang-format -i $(find $(pwd)/src -maxdepth 5 -type f -not -path '*/\.*' | grep ".[c|h][p][p]$")
-clang-format -i $(find $(pwd)/app -maxdepth 5 -type f -not -path '*/\.*' | grep ".[c|h][p][p]$")
-clang-format -i $(find $(pwd)/interface -maxdepth 5 -type f -not -path '*/\.*' | grep ".[c|h][p][p]$")
+grep_str="^.*.[cpp|hpp]$"
+
+clang-format -i $(find $(pwd)/plugins -maxdepth 5 -type f -not -path '*/\.*' | grep $grep_str)
+clang-format -i $(find $(pwd)/src -maxdepth 5 -type f -not -path '*/\.*' | grep $grep_str)
+clang-format -i $(find $(pwd)/app -maxdepth 5 -type f -not -path '*/\.*' | grep $grep_str)
+clang-format -i $(find $(pwd)/interface -maxdepth 5 -type f -not -path '*/\.*' | grep $grep_str)
