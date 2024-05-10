e23350c79cf636f5d7c19e61a139bd0479b3fcc5
Tom Mendy <tom.mendy@epitech.eu>
2024-05-10 21:17:45 +0200

    [FIX] format

--- format.sh
+++ format.sh
@@ -2,7 +2,11 @@
 
 grep_str="^.*.[cpp|hpp]$"
 
-clang-format -i $(find $(pwd)/plugins -maxdepth 5 -type f -not -path '*/\.*' | grep $grep_str)
-clang-format -i $(find $(pwd)/src -maxdepth 5 -type f -not -path '*/\.*' | grep $grep_str)
-clang-format -i $(find $(pwd)/app -maxdepth 5 -type f -not -path '*/\.*' | grep $grep_str)
-clang-format -i $(find $(pwd)/interface -maxdepth 5 -type f -not -path '*/\.*' | grep $grep_str)
+plugins_app="$(find "$(pwd)/plugins" -maxdepth 5 -type f -not -path '*/\.*' | grep "$grep_str")"
+clang-format -i $plugins_app
+src_app="$(find "$(pwd)/src" -maxdepth 5 -type f -not -path '*/\.*' | grep "$grep_str")"
+clang-format -i $src_app
+app_files="$(find "$(pwd)/app" -maxdepth 5 -type f -not -path '*/\.*' | grep "$grep_str")"
+clang-format -i $app_files
+interface_files="$(find "$(pwd)/interface" -maxdepth 5 -type f -not -path '*/\.*' | grep "$grep_str")"
+clang-format -i $interface_files
