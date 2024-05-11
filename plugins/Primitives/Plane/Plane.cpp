ba31593427aba1d5f95727814b1d2dc123ecc3f0
Marc <marc.mendia@epitech.eu>
2024-05-11 15:29:45 +0200

    [UPD] Plane rectification

--- plugins/Primitives/Plane/Plane.cpp
+++ plugins/Primitives/Plane/Plane.cpp
@@ -8,38 +8,47 @@
 #include <Plane.hpp>
 #include <cstdio>
 
-RayTracer::Plane::Plane(const char axe, double position) {
-  this->position = position;
-  this->axe = axe;
+RayTracer::Plane::Plane(const char axe,
+                        double position,
+                        std::shared_ptr<Material::Material> mat)
+{
+  this->_position = position;
+  this->_axe = axe;
+  this->_material = mat;
+
 }
 
 RayTracer::Plane::~Plane() {}
 
-bool RayTracer::Plane::hits(const RayTracer::Ray &ray, Math::Interval ray_t,
-                            Material::Material &rec) const {
-  if (axe == 'X' || axe == 'x') {
+bool RayTracer::Plane::hits(const RayTracer::Ray &ray,
+                            Math::Interval ray_t,
+                            Material::Material &rec) const
+{
+  double t;
+  if (_axe == 'X' || _axe == 'x') {
     if (ray.direction.x == 0)
-      return -1;
-    double t = (position - ray.origin._x) / ray.direction.x;
+      return false;
+    t = (_position - ray.origin._x) / ray.direction.x;
     if (t < 0)
-      return -1;
-    return t;
+      return false;
   }
-  if (axe == 'Y' || axe == 'y') {
+  if (_axe == 'Y' || _axe == 'y') {
     if (ray.direction.y == 0)
-      return -1;
-    double t = (position - ray.origin._y) / ray.direction.y;
+      return false;
+    t = (_position - ray.origin._y) / ray.direction.y;
     if (t < 0)
-      return -1;
-    return t;
+      return false;
   }
-  if (axe == 'Z' || axe == 'z') {
+  if (_axe == 'Z' || _axe == 'z') {
     if (ray.direction.z == 0)
-      return -1;
-    double t = (position - ray.origin._z) / ray.direction.z;
+      return false;
+    t = (_position - ray.origin._z) / ray.direction.z;
     if (t < 0)
-      return -1;
-    return t;
+      return false;
   }
-  return -1;
+  rec.t = t;
+  rec.p = ray.at(rec.t);
+  rec.mat = _material;
+  return true;
+  return true;
 }
